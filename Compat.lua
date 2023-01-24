1060e470c1d851cbf7c5b78fa16a6287a673b30e
Scott Mundorff <scott.mundorff@gmail.com>
2023-01-24 13:10:33 -0500

    fixes to GetItemInfoInstant, cache data

--- Compat.lua
+++ Compat.lua
@@ -1,6 +1,9 @@
-local IsLegion = select(4, GetBuildInfo()) >= 70000
+local IsWarlords    = select(4, GetBuildInfo()) >= 60000
+local IsLegion      = select(4, GetBuildInfo()) >= 70000
 
 Compat = {
+    DEBUG = false,
+
     -- https://stackoverflow.com/a/15278426
     TableConcat = function (t1,t2)
         for i=1,#t2 do
@@ -8,9 +11,42 @@ Compat = {
         end
         return t1
     end,
+    
     GetItemInfo = _G.GetItemInfo,
-    QueryAuctionItems = _G.QueryAuctionItems
+    QueryAuctionItems = _G.QueryAuctionItems,
+    
+    printd = function(...)
+        if Compat.DEBUG == true then
+            print(...)
+        end
+    end
 }
+
+local print = Compat.printd
+
+if not IsWarlords then
+    LE_ITEM_QUALITY_POOR = 0
+    LE_ITEM_QUALITY_COMMON = 1
+    LE_ITEM_QUALITY_UNCOMMON = 2
+    LE_ITEM_QUALITY_RARE = 3
+    LE_ITEM_QUALITY_EPIC = 4
+    LE_ITEM_QUALITY_LEGENDARY = 5
+    LE_ITEM_QUALITY_ARTIFACT = 6
+    LE_ITEM_QUALITY_HEIRLOOM = 7
+
+    LE_EXPANSION_CLASSIC                = 0
+    LE_EXPANSION_BURNING_CRUSADE        = 1
+    LE_EXPANSION_WRATH_OF_THE_LICH_KING = 2
+    LE_EXPANSION_CATACLYSM              = 3
+    LE_EXPANSION_MISTS_OF_PANDARIA      = 4
+    LE_EXPANSION_WARLORDS_OF_DRAENOR    = 5
+    LE_EXPANSION_LEGION                 = 6
+    LE_EXPANSION_BATTLE_FOR_AZEROTH     = 7
+    LE_EXPANSION_SHADOWLANDS            = 8
+    LE_EXPANSION_DRAGONFLIGHT           = 9
+    -- TODO set to GetBuildInfo
+    LE_EXPANSION_LEVEL_CURRENT          = 4
+end
 
 if not IsLegion then
     local GetItemInfo = _G.GetItemInfo
@@ -23,9 +59,11 @@ if not IsLegion then
     LE_ITEM_CLASS_GLYPH = 5;
     LE_ITEM_CLASS_TRADEGOODS = 6;
     LE_ITEM_CLASS_RECIPE = 7;
-    LE_ITEM_CLASS_BATTLEPET = 11;
-    LE_ITEM_CLASS_QUESTITEM = 10;
     LE_ITEM_CLASS_MISCELLANEOUS = 9;
+    LE_ITEM_CLASS_QUESTITEM = 10;
+    LE_ITEM_CLASS_BATTLEPET = 11;
+    
+    LE_ITEM_CLASS_KEY = 101;
     LE_ITEM_CLASS_ITEM_ENHANCEMENT = 100;
 
     REAGENTBANK_CONTAINER = -3;
@@ -74,24 +112,21 @@ if not IsLegion then
 	LE_INVENTORY_TYPE_CLOAK_TYPE = INVTYPE_CLOAK
 	LE_INVENTORY_TYPE_HOLDABLE_TYPE = INVTYPE_HOLDABLE
 
-    LE_ITEM_QUALITY_POOR = 0
-    LE_ITEM_QUALITY_COMMON = 1
-    LE_ITEM_QUALITY_UNCOMMON = 2
-    LE_ITEM_QUALITY_RARE = 3
-    LE_ITEM_QUALITY_EPIC = 4
-    LE_ITEM_QUALITY_LEGENDARY = 5
-    LE_ITEM_QUALITY_ARTIFACT = 6
-
     function GetItemClassIdByName(className)
-        local itemClasses = { GetAuctionItemClasses() }
+        
         if (className == "Item Enhancement") then
             return LE_ITEM_CLASS_ITEM_ENHANCEMENT
+        elseif className == "Key" then
+            return LE_ITEM_CLASS_KEY
         end
+
+        local itemClasses = { GetAuctionItemClasses() }
 
         local index={}
         for k,v in pairs(itemClasses) do
             index[v]=k
         end
+
         return index[className]
     end
 
@@ -102,23 +137,35 @@ if not IsLegion then
         for k,v in pairs(subItemClasses) do
             index[v]=k
         end
+
+        if classID == LE_ITEM_CLASS_KEY and not index["Key"] then
+            index["Key"] = 0
+        end
         return index[subClassID]
     end
 
     function GetItemClassInfo(classID)
-        local itemClasses = { GetAuctionItemClasses() };
         if (classID == LE_ITEM_CLASS_ITEM_ENHANCEMENT) then
             return "Item Enhancement";
+        elseif classID == LE_ITEM_CLASS_KEY then
+            return "Key"
         end
+
+        local itemClasses = { GetAuctionItemClasses() };
+
         return itemClasses[classID - 1];
     end
 
     function GetItemSubClassInfo(classID, subClassID)
-        --print("GetItemSubClassInfo", classID, subClassID)
+        print("GetItemSubClassInfo", classID, subClassID)
         local subItemClasses = { GetAuctionItemSubClasses(classID) }
         local index={}
-        for k,v in pairs(subItemClasses) do
-            index[v]=k
+        if type(subClassID) == "string" then
+            for k,v in pairs(subItemClasses) do
+                index[v]=k
+            end
+        elseif type(subClassID) == "number" then
+            index = subItemClasses
         end
 
         return {index[subClassID], (classID == 4 and index[subClassID] <= 4)};
@@ -170,37 +217,60 @@ if not IsLegion then
         end
 
         local classID = GetItemClassIdByName(itemType)
+        if not classID then
+            print("couldn't get class", itemID, itemType)
+            return {}
+        end
         local subClassID = GetItemSubClassIdByName(classID, itemSubType)
         --print("GetItemInfo", itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, sellPrice, classID, subClassID)
         return itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, sellPrice, classID, subClassID
     end
 
     _G.GetItemInfoInstant = function (itemID)
-        if false then
-            local data = {GetItemInfo(itemID)}
-            if not data[6] then
-                --print ("retry", itemID)
-                data = {GetItemInfo(itemID)}
-            end
+        if not itemID then return {} end
 
-            if not data[6] then
-                --print("failed to get instant data", itemID)
-                return {}
-            end
-
-            local classId = GetItemClassIdByName(data[6])
-            local subClassId = GetItemSubClassIdByName(classId, data[7])
-            -- /dump GetItemInfoInstant(6948)
-            return itemID,         -- itemid
-                data[6],    -- item type
-                data[7],    -- item subtype
-                data[9],    -- item equip location
-                GetItemIcon(itemID), -- icon
-                classId,    -- class id
-                subClassId  -- subclass id
-        else
-            return {}
+        if GetItemInfoInstantDB == nil then
+            print("init instant db")
+            GetItemInfoInstantDB = {}
         end
+
+        if GetItemInfoInstantDB[itemID] then
+            local itemType, itemSubType, itemEquipLocation, classId, subClassId = GetItemInfoInstantDB[itemID]
+            local iconPath = GetItemIcon(itemID)
+            return itemID, 
+                itemType, 
+                itemSubType, 
+                itemEquipLocation, 
+                iconPath, 
+                classId, 
+                subClassId
+        end
+
+        local data = {GetItemInfo(itemID)}
+
+        local itemType = data[6]
+        local itemSubType = data[7]
+        local itemEquipLocation = data[9]
+
+        if not itemType then
+            --print("failed to get instant data", itemID)
+            return nil, "", "", "", "", nil, nil
+        end
+
+        local classId = GetItemClassIdByName(itemType)
+        local subClassId = GetItemSubClassIdByName(classId, itemSubType)
+        
+        GetItemInfoInstantDB[itemID] = {itemType, itemSubType, itemEquipLocation, classId, subClassId}
+        --print("GetItemInfoInstantDB size", select("#", GetItemInfoInstantDB))
+        -- /dump GetItemInfoInstant(6948)
+        local iconPath = GetItemIcon(itemID)
+        return itemID, 
+            itemType, 
+            itemSubType, 
+            itemEquipLocation, 
+            iconPath, 
+            classId, 
+            subClassId
     end
 
     _G.QueryAuctionItems = function(...)
@@ -211,10 +281,10 @@ if not IsLegion then
         elseif select("#", ...) == 9 then
             if type(t[7]) == "boolean" and t[7] == true then
                 -- "", 0, 0, 0, false, 0, true, false, nil
-                --print("QueryAuctionItems", "GetAll")
+                print("QueryAuctionItems", "GetAll")
                 Compat.QueryAuctionItems("", nil, nil, 0, 0, 0, 0, 0, 0, true)
             elseif ((t[9] ~= nil and type(t[9]) == "table") or (t[4] ~= nil and type(t[4]) == "number")) then
-                --print("QueryAuctionItems", "7.x", ...)
+                print("QueryAuctionItems", "7.x", ...)
                 local text, minLevel, maxLevel, page, usable, rarity, getAll, exactMatch, filterData = ...
                 
                 local invType = nil
@@ -228,7 +298,7 @@ if not IsLegion then
                 --print('"'..text..'"', minLevel, maxLevel, invType, class, subclass, page, usable, rarity)
                 Compat.QueryAuctionItems(text, minLevel, maxLevel, invType, class, subclass, page, usable, rarity)
             else
-                --print("QueryAuctionItems", "5.x", ...)
+                print("QueryAuctionItems", "5.x", ...)
                 -- local text, minLevel, maxLevel, invType, class, subclass, page, usable, rarity = ...
                 Compat.QueryAuctionItems(...)
             end
@@ -250,7 +320,6 @@ if not IsLegion then
 
     C_TradeSkillUI = {
         GetRecipeNumItemsProduced = GetTradeSkillNumMade
-       -- GetRecipeReagentItemLink = 
     }
 
     C_WowTokenPublic = {
@@ -261,4 +330,6 @@ if not IsLegion then
     C_Social = {
         IsSocialEnabled = function() return false end
     }
+
+    _G.FlashClientIcon = function() end
 end
